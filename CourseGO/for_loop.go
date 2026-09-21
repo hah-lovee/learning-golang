edf4058019c31b88fb41222a0949fd789971a1c6
Александр <legyx1@gmail.com>
2026-09-21 15:42:28 +0300

    add loop for

---
+++ CourseGO/for_loop.go
@@ -0,0 +1,43 @@
+package coursego
+
+import "fmt"
+
+func main() {
+
+	// simple for
+	// for i := 1; i <= 5; i++ {
+	// 	fmt.Println(i)
+	// }
+
+	// iterate over collection
+	// numbers := []int{1, 2, 3, 4, 5, 6}
+	// for index, value := range numbers {
+	// 	fmt.Printf("Index: %d, Value: %d\n", index, value)
+	// }
+
+	// for i := 1; i <= 10; i++ {
+	// 	if i%2 == 0 {
+	// 		continue
+	// 	}
+	// 	fmt.Println(i)
+	// }
+
+	// rows := 5
+
+	// for i := 1; i <= rows; i++ {
+	// 	// inner loop
+	// 	for j := 1; j <= rows-i; j++ {
+	// 		fmt.Print(" ")
+	// 	}
+	// 	for k := 1; k <= 2*i-1; k++ {
+	// 		fmt.Print("*")
+	// 	}
+
+	// 	fmt.Println()
+	// }
+
+	for i := range 10 {
+		i++
+		fmt.Println(i)
+	}
+}
